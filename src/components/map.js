2012f7578694f02933aab509adcdcf689e8aca5b
melissayauri <yaurimelissa@gmail.com>
2018-03-19 10:56:27 -0500

    incorporando mapa

---
+++ src/components/map.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import HEREMap from 'react-here-maps';
+
+class Mapita extends Component {
+    render() {
+        return (
+            <HEREMap
+                appId={kNvcWGeDcX93OKDhzGXy}
+                appCode={KxJPW0n6SeLi0AFwUk0EAw}
+                center={{ lat: 0, lng: 0 }}
+                zoom={14}
+            />
+        )
+    }
+}
+
+export default Melisa
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom'
 import Header from './components/Header'
+import Mapita from './components/map'
 
 const Page = () => (
   <div>
+
   <Header/>
     <p>
       Funciona
+
     </p>
+    <Mapita/>
   </div>
+
 );
 
 render(
