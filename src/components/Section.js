ef19fe8122ce5727be56d525cc52149fe2c25a23
LilliamHaro <qualia950@gmail.com>
2018-03-19 10:42:39 -0500

    statefull

---
+++ src/components/Section.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+ const Section = ({mapa}) => (
+ 	<div>{mapa[0]}</div>
+ 	)
+
+ export default Section
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { render } from 'react-dom'
-import Header from './components/Header'
+import ReactDOM from 'react-dom';
+/* import { createStore } from 'redux'; */
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/app';
+import reducers from './reducers/index';
 
-const Page = () => (
-  <div>
-  <Header/>
-    <p>
-      Funciona
-    </p>
-  </div>
-);
 
-render(
-  <Page />,
-  document.getElementById('root')
-);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root'))
