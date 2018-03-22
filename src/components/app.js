df7772bd2a2d9492da821a159d3dfe3a004f4b4f
melissayauri <yaurimelissa@gmail.com>
2018-03-21 22:55:11 -0500

    incluyendo footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Header from './Header';
+
 import Section from '../containers/container-section';
+import Footer from './footer';
 
 const Imagen = [{name:'logito', url:'brand.png'}];
 
@@ -9,7 +11,7 @@ const App = () => (
   <div>
   <Header/>
   <Section/>
-
+  <Footer/>
   </div>
 );
 export default App
