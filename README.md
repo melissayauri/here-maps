4c41fd9defa08df16efb1278b06eba1d949ec6a7
melissayauri <yaurimelissa@gmail.com>
2018-03-22 02:20:32 -0500

    incluyendo readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # HERE MAPS
+
+## Objetivo
+Crear una plataforma que permita a los usuarios trackear y monitorear recorridos que se realice en senderos al aire libre/outdoor, orientada a los amantes de las actividades de aventura (montañas, lagos, parques, etc.) en Perú
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import Footer from './footer';
 import Header from '../containers/container-header'
 import Section from '../containers/container-section'
 import MyMap  from './map'
-
+import Footer from './footer';
 
 
 
@@ -13,10 +12,8 @@ const App = () => (
   <div>
   <Header/>
   <Section/>
-
   <MyMap />
   <Footer/>
-
   </div>
 );
 export default App
