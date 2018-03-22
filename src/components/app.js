5e491b4bb346a4d1f0ac5341803fa225344a783e
LilliamHaro <qualia950@gmail.com>
2018-03-21 22:23:58 -0500

    subiendo mapa

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
-
 import Header from '../containers/container-header'
 import Section from '../containers/container-section'
+import SimpleMap from './map'
+
 
 
 const App = () => (
   <div>
   <Header/>
   <Section/>
+  <SimpleMap/>
   </div>
 );
 export default App
--- src/components/map.js
+++ src/components/map.js
@@ -1,17 +1,35 @@
-import React, { Component } from 'react'
-import HEREMap from 'react-here-maps';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-class Mapita extends Component {
-    render() {
-        return (
-            <HEREMap
-                appId='kNvcWGeDcX93OKDhzGXy'
-                appCode='KxJPW0n6SeLi0AFwUk0EAw'
-                center={{ lat: 0, lng: 0 }}
-                zoom={14}
-            />
-        )
-    }
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDT1FgK8pARI-hv79674w5U05px8_Vt5zI'}}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
 
-export default Mapita
+export default SimpleMap;
